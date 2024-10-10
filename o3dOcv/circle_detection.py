024d64c85b92432b1697ff87d3cda4be9711a3ac
sigurd <sigurdjagerlemming@gmail.com>
2024-10-10 09:49:16 +0200

    working circle fit but not visually tested

--- o3dOcv/circle_detection.py
+++ o3dOcv/circle_detection.py
@@ -1,4 +1,4 @@
-import math
+from math import pow
 import matplotlib.pyplot as plt
 import skimage as ski
 import numpy as np
@@ -25,40 +25,38 @@ def plotImage(name):
 def objectie(x):
     pass
 
-def fitness(pi, pj, pk, edge_map):
-    xi, yi = pi
-    xj, yj = pj
-    xk, yk = pk
-    Xji = xj - xi
-    Xki = xk - xi
-    Yji = yj - yi
-    Yki = yk - yi
-    A0 = xj**2 + yj**2 - xi**2 - yi**2
-    A1 = xk**2 + yk**2 - xi**2 - yi**2
-    A = np.block([[A0, 2*Yji], [A1, 2*Yki]])
-    B = np.block([[2*Xji, A0], [2*Xki, A1]])
-    D = 4*(Xji*Yki - Xki*Yji)
+def fitness(points, edge_img):
+    x1, y1 = points[0, :]
+    x2, y2 = points[1, :]
+    x3, y3 = points[2, :]
 
-    x0 = det(A)/D
-    y0 = det(B)/D
-    r = np.sqrt((x0-xi)**2 + (y0-yi)**2)
-    print(locals())
-    coords = ski.draw.circle_perimeter(x0, y0, r)
-    return np.sum(edge_map[coords])
+    A = x1*(y2-y3) - y1*(x2-x3) + x2*y3 - x3*y2
+    B = (x1**2 + y1**2)*(y3-y2) + (x2**2 + y2**2)*(y1-y3) + (x3**2 + y3**2)*(y2-y1)
+    C = (x1**2 + y1**2)*(x2-x3) + (x2**2 + y2**2)*(x3-x1) + (x3**2 + y3**2)*(x1-x2)
+    D = (x1**2 + y1**2)*(x3*y2 - x2*y3) + (x2**2 + y2**2)*(x1*y3 - x3*y1) + (x3**2 + y3**2)*(x2*y1 - x1*y2)
+    
+    xc = np.round(-B/(2*A)).astype(int)
+    yc = np.round(-C/(2*A)).astype(int)
+    R = np.round(np.sqrt((B**2 + C**2 - 4*A*D)/(4*(A**2)))).astype(int)
+    
+    coords = ski.draw.circle_perimeter(xc, yc, R, shape=edge_img.shape)
+    return np.sum(edge_img[coords])
 
 
 image = ski.io.imread('circle_test.jpg')
 image = ski.color.rgb2gray(image)
-edges = canny(image, sigma=3)
-labeled_image, count = ski.measure.label(edges, return_num=True)
+edges_img = canny(image, sigma=3)
+labeled_image, count = ski.measure.label(edges_img, return_num=True)
 rgb_labels = ski.color.label2rgb(labeled_image, bg_label=0)
 #plotImage("rgb_labels")
 
 # POTENTIAL BUG ALLEERT, EACH RIBON MIGHT BE UNSORTED WHICH COULD CAUSE PROBLEMS WHEN FINDING THE MIDDLE OF A RIBBON
 labeled_image = np.array(labeled_image)
 edges = np.zeros((0, 2))
-for label in range(1, count+1):
+for label in range(2, count+1): # By starting at 2 we miss the background label 0 and edge caused by zero padding with label 1
     x, y = np.where(labeled_image == label)
     edges = np.block([[edges], [x[:, None], y[:, None]]])
 
-fitness(edges[10, :], edges[20, :], edges[40, :], edges)
+indices = np.random.randint(0, edges.shape[0], 3)
+fit = fitness(edges[indices, :], edges_img)
+print(fit)
