c9f70a56cba706868d030872be63c48680ae2830
sigurd <sigurdjagerlemming@gmail.com>
2024-10-24 16:39:04 +0200

    flange mount without tangential pitch

--- assets/flange_mount.py
+++ assets/flange_mount.py
@@ -3,7 +3,7 @@ from ocp_vscode import *
 import numpy as np
 
 CAM_WIDTH = 23
-CAM_LENGTH = 100
+CAM_LENGTH = 70
 CAM_HOLE_DIST = 45
 CAM_HOLE_RADIUS = 3 / 2
 MOUNT_THICKNESS = 6
@@ -12,9 +12,10 @@ FLANGE_RADIUS2 = 50 / 2
 FLANGE_HOLE_RADIUS = 6 / 2
 
 PITCH_INWARD = 45
-PITCH_SIDEWAYS = 0.00001
 
-CENTER_STICK_RADIUS = 3/2
+STICK_RADIUS = 3/2
+STICK_CLEARANCE = 0.2
+STICK_MOUNT_RADIUS = 10
 
 with BuildPart() as mount:
     with BuildSketch():
@@ -25,19 +26,15 @@ with BuildPart() as mount:
     flat = extrude(amount=MOUNT_THICKNESS)
 
     OFFSET_DOWN = 20
-    with Locations(Location((0, -(CAM_LENGTH/2 + CAM_WIDTH/2), MOUNT_THICKNESS/2 + OFFSET_DOWN), (-PITCH_INWARD, PITCH_SIDEWAYS, 0))):
+    with Locations(Location((0, -(CAM_LENGTH/2 + CAM_WIDTH/2), MOUNT_THICKNESS/2 + OFFSET_DOWN), (-PITCH_INWARD, 0, 0))):
         box = Box(CAM_LENGTH, width=CAM_WIDTH, height=MOUNT_THICKNESS, align=(Align.CENTER, Align.CENTER, Align.CENTER))
-        box_faces = box.faces().sort_by(Axis.Z)[0:3]
+        with GridLocations(x_spacing=CAM_HOLE_DIST, y_spacing=0, x_count=2, y_count=1):
+            Cylinder(CAM_HOLE_RADIUS, MOUNT_THICKNESS, mode=Mode.SUBTRACT)
+        box_faces = box.faces().sort_by(Axis.Z)[0]
     cam_block = extrude(box_faces, dir=(0, 0, -1), until=Until.LAST, target=flat)
-    # This could be replaced by a distance check from the edge of flat as well:
-    faces = cam_block.faces().filter_by(Axis.Y, tolerance=PITCH_SIDEWAYS).sort_by(Axis.Y)[0]
-    #faces = cam_block.faces().sort_by_distance((0, -CAM_LENGTH*2, 0))[0]
-    with BuildSketch(faces):
-        Rectangle(CAM_LENGTH*2, CAM_LENGTH*2)
-    extrude(amount=CAM_LENGTH*2, mode=Mode.SUBTRACT)
-    face = box.faces().sort_by()
+    faces = cam_block.faces().filter_by(Axis.Y).sort_by(Axis.Y)[0:2]
+    extrude(faces, amount=CAM_LENGTH, mode=Mode.SUBTRACT)
     
-    
-    #Cylinder(radius=CENTER_STICK_RADIUS, height=110, align=(Align.CENTER, Align.CENTER, Align.MIN))
+    Cylinder(radius=CENTER_STICK_RADIUS, height=110, align=(Align.CENTER, Align.CENTER, Align.MIN))
 
 show_all()
